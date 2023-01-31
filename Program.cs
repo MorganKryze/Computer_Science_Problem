83a0b056d0e95e54fd95308435dee607d84074de
MorganKryze <neoyega@icloud.com>
2023-01-31 18:18:59 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+
+
+namespace Computer_Science_Problem
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            Process.Start("lena.bmp");
+        }
+    }
+}
