cd425fe6cbb4085be08ef275cfcfb9f3afcc288d
MorganKryze <neoyega@icloud.com>
2023-02-11 17:59:11 +0100

    Added pixel class

---
+++ Pixel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Computer_Science_Problem
+{
+    public class Pixel
+    {
+        public byte red;
+        public byte green;
+        public byte blue;
+
+        public Pixel(byte Red, byte Green, byte Blue)
+        {
+            this.red = Red;
+            this.green = Green;
+            this.blue = Blue;
+        }
+
+    }
+}
