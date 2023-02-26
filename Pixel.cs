8aad6d6a88fc882068ef34da7e02fee40c895152
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-02-26 22:50:59 +0100

    Quick fixes on Pixel Class
    Co-authored-by: sheesh3218 <sheesh3218@users.noreply.github.com>

--- Pixel.cs
+++ Pixel.cs
@@ -9,20 +9,27 @@ namespace Computer_Science_Problem
     public class Pixel : IEquatable<Pixel>
     {
         #region Fields
+        /// <summary> This field represents the red value of the pixel. </summary>
         public byte Red;
+        /// <summary> This field represents the green value of the pixel. </summary>
         public byte Green;
+        /// <summary> This field represents the blue value of the pixel. </summary>
         public byte Blue;
         #endregion
 
         #region Constructors
+        /// <summary> This natural constructor creates a pixel with the specified values. </summary>
         public Pixel(byte red, byte green, byte blue)
         {
             Red = red;
             Green = green;
             Blue = blue;
         }
+        /// <summary> This constructor creates a black pixel. </summary>
         public Pixel() : this(0, 0, 0) { }
+        /// <summary> This constructor creates a grey pixel. </summary>
         public Pixel(byte val) : this(val, val, val) { }
+        /// <summary> This constructor creates a copy of the specified pixel. </summary>
         public Pixel(Pixel original)
         {
             Red = original.Red;
@@ -59,14 +66,13 @@ namespace Computer_Science_Problem
         /// <param name="other">The object to compare.</param>
         /// <returns>True if the object is a pixel and is equal to the current pixel, false otherwise.</returns>
         public override bool Equals(object? other) => other is Pixel && this == (Pixel)other;
+        /// <summary>This method also defines the equality between two pixels.</summary>
+        /// <param name="other">The pixel to compare.</param>
+        /// <returns>True if the pixel is equal to the current pixel, false otherwise.</returns>
+        public bool Equals(Pixel? other) => other is not null && this == other;
         ///<summary>This method gets the hash code.</summary>
         /// <returns>The hash code.</returns>
         public override int GetHashCode() => base.GetHashCode();
-
-        public bool Equals(Pixel? other)
-        {
-            return Equals(other);
-        }
         #endregion
     }
 }
