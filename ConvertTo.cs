65ab988f29e1c2bf0d1ee44c1370fe909826e773
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-02-26 22:29:12 +0100

    Added ConvertTo class that converts bytes
    Co-authored-by: sheesh3218 <sheesh3218@users.noreply.github.com>

---
+++ ConvertTo.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Computer_Science_Problem
+{
+    public static class ConvertTo
+    {
+        #region FromLittleEndian
+        /// <summary>This method converts a bytes array to a <see cref="uint"/>.</summary>
+        /// <param name="array">The bytes array.</param>
+        /// <param name="offset">The starting position.</param>
+        public static uint UInt(byte[] array, int offset = 0)
+        {
+            uint res = 0;
+            for (int i = 0; i < 4; i++)
+                res += array[offset + i] * (uint)Math.Pow(256, i);
+
+            return res;
+        }
+        /// <summary> This method converts a bytes array to an <see cref="int"/>.</summary>
+        /// <param name="array">The bytes array.</param>
+        /// <param name="offset"> The starting position</param>
+        /// <returns>An <see cref="int"/></returns>
+        public static int Int(byte[] array, int offset = 0)
+        {
+            int res = 0;
+            for (int i = 0; i < 4; i++)
+                res += array[offset + i] * (int)Math.Pow(256, i);
+
+            return res;
+        }
+
+        /// <summary> This method converts a bytes array to an <see cref="ushort"/>.</summary>
+        /// <param name="array">The bytes array.</param>
+        /// <param name="offset">The starting position.</param>
+        /// <returns>An <see cref="ushort"/>.</returns>
+        public static ushort UShort(byte[] array, int offset = 0)
+        {
+            ushort res = 0;
+            for (int i = 0; i < 2; i++)
+                res += (ushort)(array[offset + i] * Math.Pow(256, i));
+
+            return res;
+        }
+        #endregion
+
+        #region ToLittleEndian
+        /// <summary> This method converts an <see cref="int"/> to a <see cref="byte"/>array.</summary>
+        /// <param name="value"> The value to convert.</param>
+        /// <returns>A<see cref="byte"/>array.</returns>
+        public static byte[] LittleEndian(int value)
+        {
+            byte[] result = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                result[i] = (byte)(value % 256);
+                value /= 256;
+            }
+
+            return result;
+        }
+
+        /// <summary>This method converts an <see cref="uint"/> to a <see cref="byte"/> array.</summary>
+        /// <param name="value">The uint to convert</param>
+        /// <returns>A<see cref="byte"/>array.</returns>
+        public static byte[] LittleEndian(uint value)
+        {
+            byte[] result = new byte[4];
+            for (int i = 0; i < 4; i++)
+            {
+                result[i] = (byte)(value % 256);
+                value >>= 8; // value /= 256;
+            }
+
+            return result;
+        }
+
+        /// <summary>This method converts an <see cref="ushort"/> to a <see cref="byte"/> array.</summary>
+        /// <param name="value">The ushort to convert</param>
+        /// <returns>A<see cref="byte"/>array.</returns>
+        public static byte[] LittleEndian(ushort value)
+        {
+            byte[] result = new byte[2];
+            for (int i = 0; i < 2; i++)
+            {
+                result[i] = (byte)(value % 256);
+                value /= 256;
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
--- Pixel.cs
+++ Pixel.cs
@@ -3,20 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Computer_Science_Problem
 {
-    public class Pixel
+    public class Pixel : IEquatable<Pixel>
     {
-        public byte red;
-        public byte green;
-        public byte blue;
+        #region Fields
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+        #endregion
 
-        public Pixel(byte Red, byte Green, byte Blue)
+        #region Constructors
+        public Pixel(byte red, byte green, byte blue)
         {
-            this.red = Red;
-            this.green = Green;
-            this.blue = Blue;
+            Red = red;
+            Green = green;
+            Blue = blue;
         }
+        public Pixel() : this(0, 0, 0) { }
+        public Pixel(byte val) : this(val, val, val) { }
+        public Pixel(Pixel original)
+        {
+            Red = original.Red;
+            Green = original.Green;
+            Blue = original.Blue;
+        }
+        #endregion
 
+        #region Methods
+        /// <summary> This method copies the current pixel. </summary>
+        /// <returns> A copy of the current pixel. </returns>
+        public Pixel Copy()
+        {
+            return new Pixel(this);
+        }
+        /// <summary> This method returns the pixel in greyscale. </summary>
+        /// <returns> A pixel in greyscale. </returns>
+        public Pixel Greyscale() => new Pixel((byte)((Red + Green + Blue) / 3)); // m�thode en 1 ligne
+        /// <summary>This method returns a string representation of the pixel.</summary>
+        /// <returns> A string representation of the pixel. </returns>
+        public override string ToString() => $"({Red}, {Green}, {Blue})";
+        #endregion
+
+        #region Operators
+        /// <summary>This method defines the equality between two pixels.</summary>
+        /// <param name="a">The first pixel.</param>
+        /// <param name="b">The second pixel.</param>
+        public static bool operator ==(Pixel a, Pixel b) => a.Red == b.Red && a.Green == b.Green && a.Blue == b.Blue;
+        /// <summary>This method defines the inequality between two pixels.</summary>
+        /// <param name="a">The first pixel.</param>
+        /// <param name="b">The second pixel.</param>
+        public static bool operator !=(Pixel a, Pixel b) => !(a == b);
+        /// <summary>This method also defines the equality between two pixels.</summary>
+        /// <param name="other">The object to compare.</param>
+        /// <returns>True if the object is a pixel and is equal to the current pixel, false otherwise.</returns>
+        public override bool Equals(object? other) => other is Pixel && this == (Pixel)other;
+        ///<summary>This method gets the hash code.</summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode() => base.GetHashCode();
+
+        public bool Equals(Pixel? other)
+        {
+            return Equals(other);
+        }
+        #endregion
     }
 }
