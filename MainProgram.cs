6805821d4aefecac85394ed8c3a28f3d4b7ef40f
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-03-07 17:33:49 +0100

    Fixed formatting

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,70 +1,71 @@
-﻿using System.Diagnostics;
-using System;
-using System.Collections.Generic;
+﻿using static Visuals.ConsoleVisuals;
+using static Computer_Science_Problem.GeneralMethods;
 
-using static Visuals.ConsoleVisuals;
+namespace Computer_Science_Problem;
 
-using static System.Console;
-using static Computer_Science_Problem.Methods;
-
-namespace Computer_Science_Problem
+class MainProgram
 {
-    /// <summary>The main program class.</summary>
-    class MainProgram
+    #region Field
+    /// <summary>The jump variable, used as a thread to gat the information wether to continue the execution or to "jump" to a specific part of the code.</summary>
+    public static Jump jump = Jump.Continue;
+    #endregion
+
+    #region Enum
+    public enum Jump
     {
-        public static Jump jump = Jump.Continue;
-        /// <summary>The Main fonction.</summary>
-        public static void Main(string[] args)
+        Continue,
+        Main_Menu,
+        Source_Folder,
+        Actions,
+        Exit
+    }
+    #endregion
+
+    #region Main
+    public static void Main(string[] args)
+    {
+        ConsoleConfiguration();
+
+        Main_Menu:
+
+        MainMenu();
+        if (jump is not Jump.Continue) goto Select;
+        goto Actions;
+
+        Source_Folder:
+
+        string folder = ChooseFolder();
+        if (jump is Jump.Main_Menu) goto Select;
+        ChooseFile(folder);
+        goto Select;
+
+        Actions:
+
+        Actions();
+        if (jump is not Jump.Continue) goto Select;
+        goto Main_Menu;
+
+        Select:
+
+        switch (jump)
         {
-            ConsoleConfiguration();
-            
-            Main_Menu :
-
-            MainMenu();
-            if(jump is not Jump.Continue) goto Select;
-            goto Actions;
-
-            Source_Folder :
-
-            string folder = ChooseFolder();
-            if(jump is Jump.Main_Menu) goto Select;
-            ChooseFile(folder);
-            goto Select;
-
-            Actions :
-
-            Actions();
-            if(jump is not Jump.Continue) goto Select;
-            goto Main_Menu;
-
-            Select :
-
-            switch(jump)
-            {
-                case Jump.Continue: 
-                    break;
-                case Jump.Main_Menu: 
-                    jump = Jump.Continue;
-                    goto Main_Menu;
-                case Jump.Source_Folder:
-                    jump = Jump.Continue;
-                    goto Source_Folder;
-                case Jump.Actions:
-                    jump = Jump.Continue;
-                    goto Actions;
-                case Jump.Exit: 
-                    FinalExit(); 
-                    break;
-            }
-            
-        } 
-        public enum Jump
-        {
-            Continue,
-            Main_Menu,
-            Source_Folder,
-            Actions,
-            Exit
+            case Jump.Continue:
+                break;
+            case Jump.Main_Menu:
+                jump = Jump.Continue;
+                goto Main_Menu;
+            case Jump.Source_Folder:
+                jump = Jump.Continue;
+                goto Source_Folder;
+            case Jump.Actions:
+                jump = Jump.Continue;
+                goto Actions;
+            case Jump.Exit:
+                FinalExit();
+                break;
         }
     }
+    #endregion
+    
 }
+
