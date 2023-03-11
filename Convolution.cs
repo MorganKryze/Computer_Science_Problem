2c7e670725bc2cf9b390cd476b6ef28440dc5079
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-03-11 20:05:14 +0100

    Fixed running bug

--- Convolution.cs
+++ Convolution.cs
@@ -1,11 +1,15 @@
-﻿using Utilitary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Utilitary;
 
 namespace Computer_Science_Problem;
 
 /// <summary> This class represents a convolution. </summary>
 public static class Convolution
 {
-    #region Kernel Enumeration
+    #region Enumeration
     /// <summary> Enumerates the different kernels. </summary>
     public enum Kernel
     {
@@ -20,11 +24,13 @@ public static class Convolution
         /// <summary> Pushes the edges. </summary>
         EdgePushing
     }
+
     #endregion
     
-    #region Dictionary Kernel to Matrix
+    #region Dictionary
     private static Dictionary<Kernel, float[,]> kernels = new Dictionary<Kernel, float[,]>
     {
+        
         {
             Kernel.EdgeDetection, new float[,]
             {
@@ -68,55 +74,45 @@ public static class Convolution
     };
     #endregion
 
-    #region Methods
+    #region Extension methods for Image
     ///<summary> This method applies a <see cref="Kernel"/> to an <see cref="Image"/>. </summary>
     /// <param name="image"> The <see cref="Image"/> on which the kernel will be applied. </param>
     /// <param name="kernel"> The <see cref="Kernel"/> to apply. </param>
     /// <returns> An <see cref ="Image"/> where a kernel has been applied. </returns>
     public static Image ApplyKernelByName(this Image image, Kernel kernel)
     {
-        return ApplyKernelByMatrix(image, kernels[kernel]);
+        return ApplyKernel(image, kernels[kernel]);
     }
-    ///<summary> This method applies a convolution matrix to an <see cref="Image"/>. </summary>
+    ///<summary> This method applies a <see cref="Kernel"/> to an <see cref="Image"/>. </summary>
     /// <param name="image"> The <see cref="Image"/> on which the kernel will be applied. </param>
     /// <param name="kernel"> The <see cref="Kernel"/> to apply. </param>
     /// <returns> An <see cref ="Image"/> where a kernel has been applied. </returns>
-    public static Image ApplyKernelByMatrix(this Image image, float[,] kernel)
+    public static Image ApplyKernel(this Image image, float[,] kernel)
     {
-        if (kernel is null) 
-            throw new ArgumentException("kernel", "The kernel cannot be null.");
-            
-        int kernelHeight = kernel.GetLength(0);
-        int kernelWidth = kernel.GetLength(1);
+        Image newImage = new (image.Width, image.Height);
 
-        int imageHeight = image.Height;
-        int imageWidth = image.Width;
-
-        int halfKerH = kernelHeight / 2;
-        int halfKerW = kernelWidth / 2;
-
-        int resultH = imageHeight;
-        int resultW = imageWidth;
-
-        if (resultH <= 0 || resultW <= 0) 
-            throw new ArithmeticException("The size cannot be negative.");
-            
-        Image newImage = new Image(resultW, resultH);
-
-        for (int y = 0; y < resultH; y++)
+        for (int y = 0; y < image.Height; y++)
         {
-            for (int x = 0; x < resultW; x++)
+            for (int x = 0; x < image.Width; x++)
             {
                 int redFinal = 0;
                 int greenFinal = 0;
                 int blueFinal = 0;
 
-                for (int dy = 0; dy < kernelHeight; dy++)
+                for (int dy = 0; dy < kernel.GetLength(0); dy++)
                 {
-                    for (int dx = 0; dx < kernelWidth; dx++)
+                    for (int dx = 0; dx < kernel.GetLength(1); dx++)
                     {
                         int position_dy = y + dy;
                         int position_dx = x + dx;
+
+                        position_dy -= kernel.GetLength(0) / 2;
+                        position_dx -= kernel.GetLength(1) / 2;
+
+                        if (position_dy < 0 || position_dy >= image.Height)
+                            continue;
+                        if (position_dx < 0 || position_dx >= image.Width)
+                            continue;
 
                         float factor = kernel[dy, dx];
                         Pixel pixel = image[position_dx, position_dy];
@@ -130,11 +126,10 @@ public static class Convolution
                 greenFinal = Math.Min(255, Math.Max(0, greenFinal));
                 blueFinal = Math.Min(255, Math.Max(0, blueFinal));
 
-                newImage[x, y] = new Pixel((byte)redFinal, (byte)greenFinal, (byte)blueFinal);
+                newImage[x, y] = new ((byte)redFinal, (byte)greenFinal, (byte)blueFinal);
             }
         }
         return newImage;
     }
     #endregion
 }
-
